6f0650423177a19e3e4c8b9f1ec9c5b8c52134b1
Christopher H. Laco <claco@chrislaco.com>
2010-12-18 11:29:24 -0500

    Added rails site and rvmrc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TouchstartNet::Application.config.secret_token = '86f9596a8c8f2248b133d793323e67d21be4bdc2c16eeeea58d5ca54cc05a7f5b19bbc9e2be2c2bf6eeec8633acb594ffc16708623be7248ea7147ff45570df1'
