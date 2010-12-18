eb24b6266df8ed3af4f0d9eaab9e53deea985db6
Christopher H. Laco <claco@chrislaco.com>
2010-12-18 13:14:28 -0500

    Added cap env task and turned of pty

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,7 @@
 require 'bundler/capistrano'
+default_run_options[:pty] = false
+ssh_options[:forward_agent] = true
+set :use_sudo, false
 
 set :application, "touchstart.net"
 set :repository,  "git://github.com/claco/touchstart.net.git"
@@ -7,6 +10,7 @@ set :scm, :git
 set :branch, 'master'
 set :git_shallow_clone, 1
 set :copy_compression, :bz2
+set :deploy_via, :remote_cache
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "momuggs@touchstart.net"                          # Your HTTP server, Apache/etc
@@ -15,7 +19,7 @@ role :db,  "momuggs@touchstart.net", :primary => true # This is where Rails migr
 #role :db,  "your slave db-server here"
 
 set :rails_env, 'production'
-set :deploy_to, '/home/momuggs/#{application}'
+set :deploy_to, "/home/momuggs/#{application}"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
@@ -28,3 +32,8 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+task :env do
+  run 'env'
+end
+
